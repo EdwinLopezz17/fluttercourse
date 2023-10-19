4330d34acdc36fbd3a3835a7facf83c049a344aa
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-10-19 16:28:43 -0500

    class

--- lib/main.dart
+++ lib/main.dart
@@ -21,78 +21,42 @@ class MyHomePageState extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePageState> {
-  bool _subscribed = false;
+
+  Empresa _facebook = new Empresa(
+    "Meta", "Mark", 45000
+  );
+  Empresa _google = new Empresa(
+      "Google", "Larry", 75020
+  );
+  
+  @override
+  void initState(){
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Alert Dialog"),
+        title: Text("API consumption"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.redAccent,
-              ),
-              child: Text("Subscribe",
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Color(0xff445533),
-              ),),
-              onPressed: ()=>{
-                _showAlert(context),
-              },
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-                _subscribed ? "Subscribed" : " No Subscribed"
-            )
-          ],
-        ),
+        child: Text("Hello",
+        style: TextStyle(fontSize: 24),),
       )
     );
   }
 
-
-  void _showAlert(BuildContext context) {
-    showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Hello"),
-          content: Text(
-              _subscribed ? "Do you want to stop following this channel?":"Are you sure you want to subscribe?"
-          ),
-          actions: [
-            TextButton(
-              child: Text("No"),
-              onPressed: () {
-                print("No");
-                Navigator.pop(context);
-              },
-            ),
-            TextButton(
-              child: Text("Yes"),
-              onPressed: () {
-                print("Yes");
-                _subscribed = !_subscribed;
-                setState(() {
-                });
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 }
 
+class Empresa{
+  late String name;
+  late String owner;
+  late int rentYear;
 
+  Empresa(String name, String owner, int rentYear){
+    this.name = name;
+    this.owner = owner;
+    this.rentYear = rentYear;
+  }
+}
 
