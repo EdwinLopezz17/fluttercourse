45e0cc2d4d6ecd3e6b61a47558dc44c8715ebc08
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-10-19 16:05:36 -0500

    Show Alert

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:courseflutter/views/newPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,35 +21,78 @@ class MyHomePageState extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePageState> {
+  bool _subscribed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Navigation"),
+        title: Text("Alert Dialog"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Home"),
             ElevatedButton(
-              child: Text("Go to new page"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.redAccent,
+              ),
+              child: Text("Subscribe",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Color(0xff445533),
+              ),),
               onPressed: ()=>{
-                print("Presed the button"),
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context)=>NewPage())
-                )
+                _showAlert(context),
               },
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+                _subscribed ? "Subscribed" : " No Subscribed"
             )
           ],
         ),
       )
-        
-
     );
+  }
 
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Hello"),
+          content: Text(
+              _subscribed ? "Do you want to stop following this channel?":"Are you sure you want to subscribe?"
+          ),
+          actions: [
+            TextButton(
+              child: Text("No"),
+              onPressed: () {
+                print("No");
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text("Yes"),
+              onPressed: () {
+                print("Yes");
+                _subscribed = !_subscribed;
+                setState(() {
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
+
 
 
