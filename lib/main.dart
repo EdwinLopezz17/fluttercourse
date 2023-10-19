5b0f29d96ec33a46292c5a0de948dbdeb7e7bb5e
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-10-19 17:04:50 -0500

    mostrar listas

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,13 @@ class MyHomePageState extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePageState> {
 
-  Empresa _facebook = new Empresa(
-    "Meta", "Mark", 45000
-  );
-  Empresa _google = new Empresa(
-      "Google", "Larry", 75020
-  );
-  
+  List<Person> _persons = [
+    Person("Jose1", "Ramirez1", "+51 9978548561"),
+    Person("SJose2", "Ramirez2", "+51 9978548562"),
+    Person("XJose3", "Ramirez3", "+51 9978548563"),
+    Person("ZJose4", "Ramirez4", "+51 9978548564"),
+  ];
+
   @override
   void initState(){
   }
@@ -36,27 +36,82 @@ class _MyHomePageState extends State<MyHomePageState> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("API consumption"),
-      ),
-      body: Center(
-        child: Text("Hello",
-        style: TextStyle(fontSize: 24),),
+        appBar: AppBar(
+          title: Text("Lists"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+
+              },
+            ),
+          ],
+        ),
+      body: ListView.builder(
+        itemCount: _persons.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            onTap: (){
+              deletePerson(context, _persons[index]);
+            },
+            title: Text(_persons[index].name+" "+_persons[index].lastName),
+            subtitle: Text(_persons[index].phone),
+            leading: CircleAvatar(
+              child: Text(_persons[index].name.substring(0,1)),
+            ),
+            trailing: Icon(Icons.arrow_forward_ios),
+          );
+        },
       )
     );
   }
 
+  deletePerson(context,Person person) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Delete contact"),
+        content: Text("Are you sure you want to delete this contact " + person.name + "?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("Cancel"),
+          ),
+
+          TextButton(
+            onPressed: () {
+              this._persons.remove(person);
+              Navigator.pop(context);
+              setState(() {
+
+              });
+            },
+            child: Text("Delete", style: TextStyle(color: Colors.red)),
+          ),
+        ],
+      ),
+    );
+  }
+
+
 }
 
-class Empresa{
+class Person{
   late String name;
-  late String owner;
-  late int rentYear;
+  late String lastName;
+  late String phone;
 
-  Empresa(String name, String owner, int rentYear){
+  Person(name, lastName, phone){
     this.name = name;
-    this.owner = owner;
-    this.rentYear = rentYear;
+    this.lastName = lastName;
+    this.phone = phone;
   }
 }
-
